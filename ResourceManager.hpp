738ac64a847fe160a3f227c06e569e25fcd79b93
barto159 <bartosiak789@gmail.com>
2024-11-03 23:12:47 +0100

    lab2

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,44 @@
-#pragma once
-
 #include "Resource.hpp"
+#include <iostream>
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+public:
+    
+    ResourceManager() : resource(new Resource())
+    {
+        
+    }
+
+   
+    ResourceManager(const ResourceManager& other) : resource(new Resource(*other.resource))
+    {
+        
+    }
+
+   
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        
+        if (this != &other) {
+            delete resource;                          
+            resource = new Resource(*other.resource); 
+        }
+        return *this;
+    }
+
+    
+
+   
+    ~ResourceManager()
+    {
+        
+        delete resource;
+    }
+
+    
+    double get() const { return resource->get(); }
+
+private:
+    Resource* resource; 
 };
